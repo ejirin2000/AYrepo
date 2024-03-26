77e9a4d79f2c2883dd7d8ef709eaf41d947d2f5e
Ayodeji Ayorinde <ejirin2000@gmail.com>
2024-03-26 00:06:49 +0000

    our commit changes

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,9 @@
-print("Git is awesome!")
+
+# Ask the user for their name
+name = input("What is your name? ") 
+
+# Print a greeting using the provided name 
+print("Hello,", name, "! Nice to meet you!")
+
+# Print the inputted name 
+print("You entered:", name)
